bacc6bc42d1484ee255eaf8b2a99082ecbb7e0d8
vadeghar <vadeghar@gmail.com>
2024-06-26 19:04:52 +0530

    Initial commit

---
+++ src/test/java/com/azure/function/AzureTestFunctionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azure.function;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzureTestFunctionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
