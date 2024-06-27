8907c508bcbd0e5e2116359a4770b4d88b4cdfdd
vadeghar <vadeghar@gmail.com>
2024-06-27 11:51:41 +0530

    main class

--- src/main/java/com/azure/function/AzureTestFunctionApplication.java
+++ src/main/java/com/azure/function/AzureTestFunctionApplication.java
@@ -1,13 +1,43 @@
 package com.azure.function;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.info.BuildProperties;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class AzureTestFunctionApplication {
+@Slf4j
+public class AzureTestFunctionApplication implements CommandLineRunner {
+
+	@Autowired
+	BuildProperties buildProperties;
 
 	public static void main(String[] args) {
 		SpringApplication.run(AzureTestFunctionApplication.class, args);
 	}
 
+	@Bean
+	public String componentVersion() {
+		return buildProperties.getGroup() +" : "+
+				buildProperties.getArtifact() +" : "+
+				buildProperties.getName() +" : "+
+				buildProperties.getVersion() +" : "+
+				buildProperties.getTime();
+	}
+
+	@Bean
+	public ObjectMapper objectMapper() {
+		ObjectMapper objectMapper = new ObjectMapper();
+		objectMapper.findAndRegisterModules();
+		return objectMapper;
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		log.info("Application version {}", componentVersion());
+	}
 }
